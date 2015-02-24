e66ae413096c7cfe779a9f4c948f7f7fdd637e7b
Severyn Kozak <severyn.kozak@gmail.com>
2015-02-24 00:15:20 -0500

    Document script module, add better arg messages.
    livechart/script.py
    	-Add informative/descriptive messages to the argument parser.
    	-Add documentation comments to functions.

--- livechart/script.py
+++ livechart/script.py
@@ -1,12 +1,41 @@
+"""
+Contains functions for parsing command-line arguments and initializing the
+tool. Intended to serve as the command-line entry point.
+"""
+
 import argparse
 import sys
 
 from livechart import chart
 
 def parse_args():
-	parser = argparse.ArgumentParser(description="Plot a graph of STDIN data, live.")
-	parser.add_argument("-s", "--subplots", default=False, const=True, nargs="?", type=str, help="TODO")
-	parser.add_argument("-n", "--normalize", action="store_true", help="TODO")
+	"""
+	Parse command-line arguments with `argparse`, and return the resulting
+	dictionary.
+	"""
+
+	description = (
+		"Plot a graph of STDIN data, live. Pipe in either rows of "
+		"JSON-serialized dictionaries/objects or numbers. If objects are "
+		"received, each top-level key is expected to be mapped to a number."
+	)
+	parser = argparse.ArgumentParser(description=description)
+	parser.add_argument(
+		"-s", "--subplots", default=False, const=True, nargs="?", type=str,
+		help=(
+			"Whether to plot each data point on a separate graph. "
+			"`livechart` will intelligently format the subplots, but you can "
+			"specify custom dimensions in the form of a 'XxY' string (eg '5x6')."
+		)
+	)
+	parser.add_argument(
+		"-n", "--normalize", action="store_true",
+		help=(
+			"Whether data points should be normalized. May be desirable when "
+			"points with vastly different ranges are getting plotted on the "
+			"same graph."
+		)
+	)
 
 	args = vars(parser.parse_args())
 	if not args["subplots"]:
@@ -28,8 +57,12 @@ def parse_args():
 	return args
 
 def run():
+	"""
+	Runs the tool.
+	"""
+
 	chart.configure_pyplot()
 	try:
-		chart.render_stdin(parse_args())
+		sys.exit(chart.render_stdin(parse_args()))
 	except KeyboardInterrupt:
-		sys.exit(0)
+		pass
